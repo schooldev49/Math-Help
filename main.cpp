9d97fa366b8b44dec9febb6d0eaedc0915f0feb7
schooldev49 <102310270+schooldev49@users.noreply.github.com>
2022-11-03 21:44:10 -0400

    user input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
+
 #include <iostream>
 #include <vector>
+#include <string>
 #include "Math.hpp"
-
+#include <sstream>
+enum questionType {
+  meanAbs
+};
 
 int main(){
-  std::vector<double> inputs {5,7,8,2,2};
+  
+  std::string s;
+  std::vector<double> inputs;
+  std::cout << "Enter the digits in the data, seperated by commas: ";
+  std::cin >> s;
+  std::stringstream ss(s);
+  while (ss.good()){
+    std::string substring;
+    std::getline(ss,substring,',');
+    inputs.push_back(std::stod(substring));
+  }
   Math math;
- 
-  std::cout << math.meanAbs(inputs) << "\n";
+ std::cout << math.meanAbs(inputs) << "\n";
   return 0;
 }
